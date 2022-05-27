230a815425005474bbfc452cd66e249ca2a1b0e6
Olivia WALSH <owalsh@e2r7p16.clusters.42paris.fr>
2022-05-27 14:54:06 +0200

    Add Nothing

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,16 +6,25 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 12:56:02 by owalsh            #+#    #+#             */
-/*   Updated: 2022/05/26 15:50:31 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/05/27 11:44:03 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
 # include <stdio.h>
 # include <unistd.h>
 # include <mlx.h>
+# include "minilibx-linux/mlx.h"
 # include "libft/libft.h"
 # include "get_next_line/get_next_line.h"
 
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 12:56:06 by owalsh            #+#    #+#             */
-/*   Updated: 2022/05/26 17:12:01 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/05/27 14:45:12 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,30 @@ void	error()
 	ft_putstr("Error\n");
 }
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(int argc, char **argv)
 {
-	void	*ptr;
+	(void)argc;
+	(void)argv;
+	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
-
-	(void)ptr;
-	(void)mlx_win;
-
-
-	ptr = mlx_init();
-	mlx_win = mlx_new_window(ptr, 1920, 1080, "Hello world!");
-	mlx_loop(ptr);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 	
-	if (argc == 2)
-	{
-		(void)argv;
-	}
-	else
-		error();
-
 	return (0);
 }
